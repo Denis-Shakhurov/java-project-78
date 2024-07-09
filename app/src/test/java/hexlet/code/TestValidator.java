03a9c0c48384ca0e70b83657c25c1ff8192d2a45
DenisShakhurov <disa.sdf86@gmail.com>
2024-07-09 09:55:30 +0700

    update TestValidator.java

--- app/src/test/java/hexlet/code/TestValidator.java
+++ app/src/test/java/hexlet/code/TestValidator.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestValidator {
     private Validator v = new Validator();
-    private StringSchema schema = v.string();
 
     @Test
     public void testStringSchema() {
+        StringSchema schema = v.string();
         assertFalse(schema.required().isValid(""));
         assertTrue(schema.minLength(3).contains("ol").isValid("cool"));
         assertFalse(schema.minLength(3).minLength(10).minLength(12).isValid("Hexlet"));
+    }
+
+    @Test
+    public void testNumberSchema() {
+        NumberSchema schema = v.number();
+        assertTrue(schema.isValid(5));
+        assertTrue(schema.isValid(null));
+        assertFalse(schema.required().isValid(null));
+        assertFalse(schema.positive().isValid(-5));
+        assertFalse(schema.isValid(0));
+        assertTrue(schema.range(2, 9).isValid(4));
+        assertTrue(schema.isValid(7));
     }
 }
