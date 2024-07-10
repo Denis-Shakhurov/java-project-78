5a17dc3f6bb8487a58dcc831343493363bef0307
DenisShakhurov <disa.sdf86@gmail.com>
2024-07-10 16:45:31 +0700

    refactoring BaseSchema.java

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,14 +1,29 @@
 package hexlet.code.schemas;
 
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class BaseSchema<T> {
+    protected List<Predicate<T>> predicateList = new LinkedList<>();
 
-    public boolean isValid(T value) {
-        return true;
+    public BaseSchema<T> required() {
+        Predicate<T> predicateRequired = val -> val != null;
+        predicateList.remove(predicateRequired);
+        predicateList.add(predicateRequired);
+        return this;
     }
 
-    protected void reset(Map<String, T> map) {
-        map.clear();
+    public boolean isValid(T value) {
+        int res = 0;
+        for (Predicate<T> predicate : predicateList) {
+            if (!predicate.test(value)) {
+                res += 1;
+                break;
+            } else {
+                res += 2;
+            }
+        }
+        return res % 2 == 0;
     }
 }
