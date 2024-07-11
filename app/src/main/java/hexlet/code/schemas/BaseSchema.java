46f380f5e1ed3d617c01faf5a8970657bde7b5ab
DenisShakhurov <disa.sdf86@gmail.com>
2024-07-11 16:01:54 +0700

    refactoring BaseSchema.java

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,22 +6,21 @@ import java.util.function.Predicate;
 
 public class BaseSchema<T> {
     protected Map<String, Predicate<T>> predicates = new LinkedHashMap<>();
-
-    public BaseSchema<T> required() {
-        Predicate<T> predicateRequired = val -> val != null;
-        predicates.put("required", predicateRequired);
-        return this;
-    }
+    protected boolean requiredStatus = false;
 
     public boolean isValid(T value) {
         int res = 0;
-        var keys = predicates.keySet();
-        for (var key : keys) {
-            if (!predicates.get(key).test(value)) {
-                res += 1;
-                break;
-            } else {
-                res += 2;
+        if (value == null) {
+            res += requiredStatus ? 1 : 2;
+        } else {
+            var keys = predicates.keySet();
+            for (var key : keys) {
+                if (!predicates.get(key).test(value)) {
+                    res += 1;
+                    break;
+                } else {
+                    res += 2;
+                }
             }
         }
         return res % 2 == 0;
