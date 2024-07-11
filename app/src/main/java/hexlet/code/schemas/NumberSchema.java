329518b29aec538849b4510cc7557cb956b5651c
DenisShakhurov <disa.sdf86@gmail.com>
2024-07-11 09:25:49 +0700

    refactoring NumberSchema.java

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -3,17 +3,19 @@ package hexlet.code.schemas;
 import java.util.function.Predicate;
 
 public class NumberSchema extends BaseSchema<Integer> {
+    public NumberSchema() {
+        predicates.clear();
+    }
 
     public NumberSchema positive() {
         Predicate<Integer> positivePredicate = number -> number > 0;
-        predicateList.add(positivePredicate);
+        predicates.put("positive", positivePredicate);
         return this;
     }
 
     public NumberSchema range(int from, int before) {
         Predicate<Integer> rangePredicate = number -> number >= from || number <= before;
-        predicateList.remove(rangePredicate);
-        predicateList.add(rangePredicate);
+        predicates.put("range", rangePredicate);
         return this;
     }
 }
